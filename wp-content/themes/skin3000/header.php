fe8065d409a6b406a3c148a90250821408d00ec0
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-18 22:16:34 +0600

    main file splitted to header & footer

---
+++ wp-content/themes/skin3000/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title><?php wp_get_document_title() ?></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <?php wp_head() ?>
+</head>
+<body>
+<div class="wrapper">
+    <header class="main-header">
+        <div class="top-header">
+            <div class="top-header__wrap">
+                <div class="logotype-block">
+                    <div class="logo-wrap"><a href="<?php echo site_url() ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.svg" alt="<?php bloginfo('name') ?>" class="logo-wrap__logo-img"></a></div>
+                </div>
+                <?php
+                wp_nav_menu([
+                    'theme_location' => 'topmenu',
+                    'container' => 'nav',
+                    'container_class' => 'main-navigation',
+                    'menu_class' => 'nav-list',
+                    'depth' => 1,
+                ]);
+                ?>
+            </div>
+        </div>
+        <div class="bottom-header">
+            <div class="search-form-wrap">
+                <form class="search-form">
+                    <input type="text" placeholder="Поиск..." class="search-form__input">
+                    <button class="search-form__btn-search"><i class="icon icon-search"></i></button>
+                </form>
+            </div>
+        </div>
+    </header>
+    <!-- header_end-->
--- wp-content/themes/skin3000/index.php
+++ wp-content/themes/skin3000/index.php
@@ -1,42 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <title><?php wp_get_document_title() ?></title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-    <?php wp_head() ?>
-  </head>
-  <body>
-    <div class="wrapper">
-      <header class="main-header">
-        <div class="top-header">
-          <div class="top-header__wrap">
-            <div class="logotype-block">
-              <div class="logo-wrap"><a href="<?php echo site_url() ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.svg" alt="<?php bloginfo('name') ?>" class="logo-wrap__logo-img"></a></div>
-            </div>
-            <?php
-            wp_nav_menu([
-                'theme_location' => 'topmenu',
-                'container' => 'nav',
-                'container_class' => 'main-navigation',
-                'menu_class' => 'nav-list',
-                'depth' => 1,
-            ]);
-            ?>
-          </div>
-        </div>
-        <div class="bottom-header">
-          <div class="search-form-wrap">
-            <form class="search-form">
-              <input type="text" placeholder="Поиск..." class="search-form__input">
-              <button class="search-form__btn-search"><i class="icon icon-search"></i></button>
-            </form>
-          </div>
-        </div>
-      </header>
-      <!-- header_end-->
+<?php get_header() ?>
       <div class="main-content">
         <div class="content-wrapper">
           <div class="content">
@@ -156,24 +118,4 @@
           </div>
         </div>
       </div>
-      <footer class="main-footer">
-        <div class="content-footer">
-            <?php
-            wp_nav_menu([
-                'theme_location' => 'botmenu',
-                'container_class' => 'bottom-menu',
-                'menu_class' => 'b-menu__list',
-                'depth' => 1,
-            ]);
-            ?>
-            <div class="copyright-wrap">
-                <div class="copyright-text">Туристик<a href="#" class="copyright-text__link"> loftschool <?php echo date('Y') ?></a></div>
-            </div>
-        </div>
-      </footer>
-    </div>
-    <!-- wrapper_end-->
-
-    <?php wp_footer() ?>
-  </body>
-</html>
+<?php get_footer() ?>
